49d84fbdeb21c9e78e01e64d9dd4a4f437da90ca
Holden Karau <holden@pigscanfly.ca>
2014-04-23 14:33:10 -0700

    extra param to fprintf

--- src/cronotest.c
+++ src/cronotest.c
@@ -225,7 +225,7 @@ main(int argc, char **argv, char **envp)
 	if (   (period_delay_units > periodicity)
 	    || (   period_delay_units == periodicity
 		&& abs(period_delay)  >= period_multiple)) {
-	    fprintf(stderr, "%s: period delay cannot be larger than the rollover period\n", argv[0], start_time);
+	    fprintf(stderr, "%s: period delay cannot be larger than the rollover period\n", argv[0]);
 	    exit(1);
 	}		
 	period_delay *= period_seconds[period_delay_units];
